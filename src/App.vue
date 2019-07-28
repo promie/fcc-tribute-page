efa167f4a8e153a3ca4ced8a1e839fd943caa721
Promie Yutasane <promie.yutasane@gmail.com>
2019-07-28 16:42:38 +1000

    Initiating Project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <TributePage title="Evan You"/>
+  </div>
+</template>
+
+<script>
+import TributePage from './components/TributePage'
+
+export default {
+  name: 'app',
+  components: {
+    TributePage
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/TributePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>Tribute To {{ title }}</h1>
+    <p>The mastermind behind VueJS</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TributePage',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
